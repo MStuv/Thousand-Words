9d01f6b4f711034174c8cce55b6616c779beea72
MStuv <markstuver@MacBook-Pro.local>
2013-12-02 23:05:08 -0500

    Initial Commit

---
+++ Thousand Words/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by Mark Stuver on 12/2/13.
+//  Copyright (c) 2013 Halo International Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
