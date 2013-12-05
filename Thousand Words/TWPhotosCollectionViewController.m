b96a71c6a3f0fd34130a908b155940005abdecc6
MStuv <markstuver@MacBook-Pro.local>
2013-12-04 22:00:28 -0500

    9.3 - 9.4 Creating and Storing Our Photos

--- Thousand Words/TWPhotosCollectionViewController.m
+++ Thousand Words/TWPhotosCollectionViewController.m
@@ -8,6 +8,9 @@
 
 #import "TWPhotosCollectionViewController.h"
 #import "TWPhotoCollectionViewCell.h"
+#import "Photo.h"
+#import "TWPictureDataTransformer.h"
+#import "TWCoreDataHelper.h"
 
 /// Privately... Conform to ImagePickerVC & NavigationC Delegates
 @interface TWPhotosCollectionViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
@@ -67,46 +70,85 @@
     [self presentViewController:picker animated:YES completion:nil];
 }
 
+#pragma mark - Helper Methods
+
+/// method will take a UIImage and Return a Photo instance
+-(Photo *)photoFromImage:(UIImage *)image
+{
+    /// Create instance of Photo and set to the CoreData ManagedObject *see TWAlbumTableViewController.m/albumWithName: method
+    Photo *photo = [NSEntityDescription insertNewObjectForEntityForName:@"Photo" inManagedObjectContext:[TWCoreDataHelper managedObjectContext]];
+    
+    /// set Photo instance's image property to the UIImage passed to this method
+    photo.image = image;
+    /// set Photo instance's date property to the current date
+    photo.date = [NSDate date];
+    /// set Photo instance's albumBook property
+    photo.albumBook = self.album;
+    
+    /// Create instance of NSError and set to nil
+    NSError *error = nil;
+    /// if there is a problem, save the error to the NSError instance so that it can be looked at and debugged.
+    if (![[photo managedObjectContext] save:&error]) {
+        // Error in saving
+        NSLog(@"%@", error);
+    }
+    return photo;
+}
+
 
 
 #pragma mark - UICollectionView DataSource
 
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
+    /// Set the CellIdentifier
     static NSString *CellIdentifier = @"Photo Cell";
     
+    /// Create instance of TWPhotoCollectionViewCell and set it to as the dequeueReusableCell at the indexPath
     TWPhotoCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:CellIdentifier forIndexPath:indexPath];
     
+    /// Create instance of Photo and set to the object in photos array at indexPath.row
+    Photo *photo = self.photos[indexPath.row];
+    
+    /// set color of cell's background to white
     cell.backgroundColor = [UIColor whiteColor];
-    cell.imageView.image = self.photos[indexPath.row];
+    
+    /// set cell imageView's image to image property of photo instance
+    cell.imageView.image = photo.image;
     
     return cell;
 }
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
+    /// return count of mutableArray
     return [self.photos count];
 }
 
 
 #pragma mark - UIImagePickerController Delegate
 
+///DID FINISH PICKING
 -(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
-    /// Set UIImage instance to the photo at a specific
+    /// Set UIImage instance to the ImagePicker Edited Image
     UIImage *image = info[UIImagePickerControllerEditedImage];
     
+     /// if the image is nil (no edited Image)... use originalImage
     if (!image) {
-        
         image = info[UIImagePickerControllerOriginalImage];
     }
+    /// addObject to mutableArray via the helper method 'photoFromImage:' passing the image instance.
+    [self.photos addObject:[self photoFromImage:image]];
     
-    [self.photos addObject:image];
-    
+    /// reload collectionView
     [self.collectionView reloadData];
+    
+    /// dismissVC
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+/// DID CANCEL PICKER
 -(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
 {
     
