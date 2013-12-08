354c9f3e3cd814dacff3344e4e87df62f25039e2
MStuv <markstuver@MacBook-Pro.local>
2013-12-08 00:26:27 -0500

    9.13 - 9.15 Adding Filters, Pass the Filters & Saving the Filters

--- Thousand Words/TWFiltersCollectionViewController.m
+++ Thousand Words/TWFiltersCollectionViewController.m
@@ -14,17 +14,23 @@
 
 /// Private Property for this class only
 @property (strong, nonatomic) NSMutableArray *filters;
+@property (strong, nonatomic) CIContext *context;
 
 @end
 
 @implementation TWFiltersCollectionViewController
 
-/// lazy instantiation of filters array
+/// lazy instantiations
 -(NSMutableArray *)filters
 {
     if(!_filters) _filters = [[NSMutableArray alloc] init];
-    
     return _filters;
+}
+
+-(CIContext *)context
+{
+    if (!_context) _context = [CIContext contextWithOptions:nil];
+    return _context;
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -40,12 +46,70 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    /// Load mutableArray filters with a mutableCopy of the array that is returned from the photoFilters class method
+    self.filters = [[[self class] photoFilters] mutableCopy];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Helper Methods
+
+/// This is made as a Class Method to give future flexability to use in other controllers
++(NSArray *)photoFilters
+{
+    CIFilter *sepia = [CIFilter filterWithName:@"CISepiaTone" keysAndValues:nil];
+    
+    CIFilter *blur = [CIFilter filterWithName:@"CIGaussianBlur" keysAndValues:nil];
+    
+    CIFilter *colorClamp = [CIFilter filterWithName:@"CIColorClamp" keysAndValues:@"inputMaxComponents", [CIVector vectorWithX:0.9 Y:0.9 Z:0.9 W:0.9], @"inputMinComponents", [CIVector vectorWithX:0.2 Y:0.2 Z:0.2 W:0.2], nil];
+    CIFilter *instant = [CIFilter filterWithName:@"CIPhotoEffectInstant" keysAndValues:nil];
+    
+    CIFilter *noir = [CIFilter filterWithName:@"CIPhotoEffectNoir" keysAndValues:nil];
+    
+    CIFilter *vignette = [CIFilter filterWithName:@"CIVignetteEffect" keysAndValues:nil];
+    
+    CIFilter *colorControls = [CIFilter filterWithName:@"CIColorControls" keysAndValues:kCIInputSaturationKey, @0.5, nil];
+    
+    CIFilter *transfer = [CIFilter filterWithName:@"CIPhotoEffectTransfer" keysAndValues:nil];
+    
+    CIFilter *unsharpen = [CIFilter filterWithName:@"CIUnsharpMask" keysAndValues:nil];
+    
+    CIFilter *monochrome = [CIFilter filterWithName:@"CIColorMonochrome" keysAndValues:nil];
+    
+    NSArray *allFilters = @[sepia, blur, colorClamp, instant, noir, vignette, colorControls, transfer, unsharpen, monochrome];
+    
+    return allFilters;
+}
+
+/// Method That Applies the passed filter to the passed image
+-(UIImage *)filteredImageFromImage:(UIImage *)image andFilter:(CIFilter *)filter
+{
+    /// Convert UIImage to an instance of CIImage
+    CIImage *unfilteredImage = [[CIImage alloc] initWithCGImage:image.CGImage];
+    
+    /// set up filter
+    [filter setValue:unfilteredImage forKey:kCIInputImageKey];
+    
+    /// apply filter to image using the CIImage method outputImage and set to value of instance of CIImage
+    CIImage *filteredImage = [filter outputImage];
+    
+    /// create an instance of CGRect and set it to the size of the image
+    CGRect extent = [filteredImage extent];
+    
+    /// convert the CIImage to a CGImageRef
+    CGImageRef cgImage = [self.context createCGImage:filteredImage fromRect:extent];
+    
+    /// convert CGImage to an instance of UIImage
+    UIImage *finalImage = [UIImage imageWithCGImage:cgImage];
+    
+    /// return UIImage instance with final filter
+    return finalImage;
 }
 
 #pragma mark - UICollectionView DataSource
@@ -57,7 +121,8 @@
     TWPhotoCollectionViewCell *cell =[collectionView dequeueReusableCellWithReuseIdentifier:CellIdentifier forIndexPath:indexPath];
     
     cell.backgroundColor = [UIColor whiteColor];
-    cell.imageView.image = self.photo.image;
+    cell.imageView.image = [self filteredImageFromImage:self.photo.image andFilter:self.filters[indexPath.row]];
+    
     
     return cell;
 }
@@ -66,5 +131,25 @@
 {
     return [self.filters count];
 }
+
+
+#pragma mark - UICollectionView Delegate
+
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    /// What cell did the user select
+    TWPhotoCollectionViewCell *selectedCell = (TWPhotoCollectionViewCell *) [collectionView cellForItemAtIndexPath:indexPath];
+    
+    self.photo.image = selectedCell.imageView.image;
+    
+    NSError *error = nil;
+    
+    if (![[self.photo managedObjectContext] save:&error]) {
+        NSLog(@"%@", error);
+    }
+    
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
 
 @end
